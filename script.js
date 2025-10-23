6c1f2e031f7ee3ec41b5c501c7cd60952ac07699
Moeenc137 <moeenc137@gmail.com>
2025-10-23 15:18:39 +0500

    javascript prctc(loops)

---
+++ script.js
@@ -0,0 +1,25 @@
+// // loops:
+
+// // for-loop:
+
+let num = "moin";
+
+for (let i = 1; i <= 5; i++) {
+  console.log(num);
+}
+console.log("loop has ended");
+
+// // while loop:
+let i = 1;
+while (i <= 5) {
+  console.log(i);
+  i++;
+}
+
+// for-while loop:
+
+let ii = 1;
+do {
+  console.log("loop");
+  i++;
+} while (i <= 5);
