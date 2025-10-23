21520e6a862c35046c9c260b0873910813ae00af
Moeenc137 <moeenc137@gmail.com>
2025-10-23 19:12:00 +0500

    Javascript practice(loops completed)

--- script.js
+++ script.js
@@ -16,10 +16,58 @@ while (i <= 5) {
   i++;
 }
 
-// for-while loop:
+// do-while loop:
 
 let ii = 1;
 do {
   console.log("loop");
   i++;
 } while (i <= 5);
+
+//for-of loop:
+
+let str = "javascript";
+let size = 0;
+for (let il of str) {
+  console.log(il);
+  size++;
+}
+console.log(size);
+
+//for-in loop:
+
+let student = {
+  name: "Bilal",
+  uni: "LUMS",
+  city: "Lahore",
+  age: 22,
+  cgpa: 3.5,
+};
+
+for (let st in student) {
+  console.log(st, student[st]);
+}
+
+// if (even % 2 === 0) {
+//   console.log("the number is even");
+// } else {
+//   console.log("num is not even");
+// }
+
+// practice(for-loop):  sort all even values from 0-100
+
+for (let i = 2; i <= 100; i++) {
+  if (i % 2 === 0) {
+    console.log("i:", i);
+  }
+}
+
+// practice : ask user for numbers until user give the right answer.
+
+let gamenum = 25;
+let asknum = prompt("Enter the number");
+
+while (gamenum != asknum) {
+  gamenum = prompt("try again");
+}
+console.log("Right number");
